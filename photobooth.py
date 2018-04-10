468e83d02065b81133bd4525ff2a9638701ebfdf
Timothy Mwiti <mwititimothyk@gmail.com>
2018-04-10 10:31:18 -0500

    addition of capability to start recording upon face detection

--- photobooth.py
+++ photobooth.py
@@ -1,32 +1,96 @@
 from picamera import PiCamera
+from respeaker3 import MicArray
+from threading import Thread
 import time
+import sys
 import cv2
-from respeaker import MicArray
+import os
 
 camera = PiCamera()
+def setup_time():
+        try:
+                client = ntplib.NTPClient()
+                response = client.request('pool.ntp.org')
+                print time.localtime(response.tx_time)
+                os.system('date ' + time.strftime('%m%d%H%M%Y.%S', time.localtime(response.tx_time)))
+        except:
+                print('Could not sync with time server.')
+
+        print('Done.')
 
 
-# capture image
 
-def capture_image(image_name='foo.jpg'):
-	global camera
-	camera.start_preview()
-	time.sleep(2)
-	camera.capture(image_name)
+def detect_face(image_path = 'foo.jpeg', casc_path = 'haarcascade_frontalface_default.xml'):
+        face_cascade = cv2.CascadeClassifier(casc_path)
+        face_found = False
+        global camera
+        if camera.closed:
+                camera = PiCamera()
+        print 'finding faces in frame...'
+        while not face_found:
+                capture_image()
+                image = cv2.imread(image_path)
+                gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+                # Detect faces in the image
+                faces = face_cascade.detectMultiScale(
+                        gray,
+                        scaleFactor=1.1,
+                        minNeighbors=5,
+                        minSize=(30, 30)
+                        )
+                if len(faces) > 0:
+                        face_found = True
+                        camera.close()
+                else:
+                        print 'removing image'
+                        os.remove(image_path)
+        for (x, y, w, h) in faces:
+                cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        cv2.imshow("Face found", image)
+        cv2.waitKey(3000)
+        os.remove(image_path)
+        return
+
+        
+def capture_image(image_name='foo.jpeg'):
+        global camera
+        if camera.closed:
+                print 'Camera is opening...'
+                camera = PiCamera()
+        camera.capture(image_name)
 
 
 # capture video file
 
-def capture_video(recording_time=10, recording_name='foo.h264', start_time=time.time()):
+def capture_video(video_length=10, recording_name='foo.h264', start_time=time.time()):
 	global camera
-	if time.time() >= start_time:
-		camera.start_recording(recording_name)
-		camera.wait_recording(recording_time)
-		camera.stop_recording()
+	if camera.closed:
+                print 'Camera is opening...'
+                camera = PiCamera()
+	while True:
+                if time.time() >= start_time+1:
+                        print 'Time started recording video: ', time.time()
+                        camera.start_recording(recording_name)
+                        camera.wait_recording(video_length)
+                        camera.stop_recording()
+                        print 'finished recording video ', time.time()
+                        break
 
-def record_video_with_audio(length=10):
+def record_video_with_audio(length=60,start_delay=8.0,on_face_detect=False):
 	sd = MicArray()
-	start_time = time.time() + 10
-	capture_video(start_time=start_time,recording_time=length)
-	sd.run(start_time=start_time, audio_length=length)
+	if on_face_detect:
+                detect_face()
+                start_time = time.time()+start_delay
+        else:
+                start_time = time.time()+start_delay
+	t1 = Thread(target=capture_video, kwargs={'start_time':start_time, 'video_length':length})
+	t2 = Thread(target=sd.run, kwargs={'start_time':start_time, 'audio_length':length})
+	t1.start()
+	t2.start()
+	t1.join()
+	t2.join()
+
+if __name__ == '__main__':
+        record_video_with_audio(length=30,on_face_detect=True)
 
--- respeaker.py
+++ respeaker.py
@@ -1,86 +1,32 @@
 import pyaudio
 import Queue
-import numpy as np
-from gcc_phat import gcc_phat
 import math
 import audioop
 import time
 import wave
 from collections import deque
-from pixels import pixels
-import csv
 import sys
 import os
-import ntplib
 
 
 SOUND_SPEED = 340
 MIC_DISTANCE_4 = 0.081
 MAX_TDOA_4 = MIC_DISTANCE_4 / float(SOUND_SPEED)
-DIRECTIONS_QUEUE = Queue.Queue()
-AUDIO_QUEUE = Queue.Queue()
-
-try:
-	client = ntplib.NTPClient()
-	response = client.request('pool.ntp.org')
-	print time.localtime(response.tx_time)
-	os.system('date ' + time.strftime('%m%d%H%M%Y.%S', time.localtime(response.tx_time)))
-except:
-	print('Could not sync with time server.')
-
-print('Done.')
 
 
 class MicArray(object):
-	def __init__(self, rate=16000, channels=4, chunk_size=None):
+	def __init__(self, rate=44100, channels=4, chunk_size=2048):
 		self.FORMAT = pyaudio.paInt16
 		self.CHANNELS = channels
 		self.RATE = rate
 		self.CHUNK = chunk_size if chunk_size else 1024
 		self.pyaudio_instance = pyaudio.PyAudio()
-		self.SILENCE_LIMIT = 5
-		self.PREV_AUDIO = 0.5
-		self.THRESHOLD = 800
 		self.queue = Queue.Queue()
-
-	# Need to sort out setting threshold for 4 mic array
-	def setup_mic(self, num_samples=50):
-		# Gets average audio intensity of your mic sound.
-		print "Getting intensity values from mic."
-		device_index = None
-		for i in range(self.pyaudio_instance.get_device_count()):
-			dev = self.pyaudio_instance.get_device_info_by_index(i)
-			name = dev['name'].encode('utf-8')
-			if dev['maxInputChannels'] == self.CHANNELS:
-				device_index = i
-				break
-
-		if device_index is None:
-			raise Exception('can not find input device with {} channel(s)'.format(self.CHANNELS))
-
-		p = pyaudio.PyAudio()
-		stream = p.open(
-			input=True,
-			format=self.FORMAT,
-			channels=self.CHANNELS,
-			rate=self.RATE,
-			frames_per_buffer=self.CHUNK,
-			input_device_index=device_index,
-		)
-		values = [
-			math.sqrt(abs(audioop.avg(stream.read(self.CHUNK), 4)))
-			for x in range(num_samples)]
-		values = sorted(values, reverse=True)
-		r = sum(values[:int(num_samples * 0.2)]) / int(num_samples * 0.2)
-		print " Finished getting intensity values from mic"
-		stream.close()
-		p.terminate()
-		return r
 
 	# Save recorded speech to a wav file.
 	def save_speech(self, data):
 		p = self.pyaudio_instance
-		filename = int(time.time())
+		filename = str(time.time())
 		data = ''.join(data)
 		wf = wave.open('./audio_files/' + filename + '.wav', 'wb')
 		wf.setnchannels(4)
@@ -94,8 +40,8 @@ class MicArray(object):
 		p = self.pyaudio_instance
 		device_index = None
 		print 'Setting up pyaudio ...'
-		for i in range(self.pyaudio_instance.get_device_count()):
-			dev = self.pyaudio_instance.get_device_info_by_index(i)
+		for i in range(p.get_device_count()):
+			dev = p.get_device_info_by_index(i)
 			name = dev['name'].encode('utf-8')
 			if dev['maxInputChannels'] == self.CHANNELS:
 				print('Using {}'.format(name))
@@ -104,39 +50,37 @@ class MicArray(object):
 
 		if device_index is None:
 			raise Exception('can not find input device with {} channel(s)'.format(self.CHANNELS))
-
-		stream = p.open(
-			input=True,
-			format=self.FORMAT,
-			channels=self.CHANNELS,
-			rate=int(self.RATE),
-			frames_per_buffer=int(self.CHUNK),
-			input_device_index=device_index,
-		)
-		print "* Listening mic..."
 		audio2send = []
 		cur_data = ''
-		rel = int(self.RATE / self.CHUNK)
-		slid_win = deque(maxlen=self.SILENCE_LIMIT * rel)
-
-		# Prepend audio from 0.5 seconds before noise was detected
-		prev_audio = deque(maxlen=self.PREV_AUDIO * rel)
+		rel = self.RATE / self.CHUNK
 		started = False
 		response = []
-		if time.time() >= start_time():
-			while time.time()-start_time < audio_length:
-				try:
-					cur_data = stream.read(self.CHUNK)
-					slid_win.append(math.sqrt(abs(audioop.avg(cur_data, 3))))
-					audio2send.append(cur_data)
-				except KeyboardInterrupt:
-					self.save_speech(audio2send)
-
-		self.save_speech(audio2send)
-		print "* Done recording...Exiting..."
-		stream.close()
-		p.terminate()
-
+		while True:
+                        if time.time() >= start_time:
+                                print 'Opening stream ', time.time()
+                                stream = p.open(
+                                        input=True,
+                                        format=self.FORMAT,
+                                        channels=self.CHANNELS,
+                                        rate=int(self.RATE),
+                                        frames_per_buffer=self.CHUNK,
+                                        input_device_index=device_index,)
+                                print 'Stream is now open ', time.time()
+                                
+                                cur_data = stream.read(self.CHUNK)
+                                for i in range(0, int(rel * audio_length)):
+                                        try:
+                                                cur_data = stream.read(self.CHUNK, False)
+                                        except IOError as ex:
+                                                continue
+                                                
+                                        audio2send.append(cur_data)
+                                        print i
+                                print 'Finished recording audio ', time.time()
+                                self.save_speech(audio2send)
+                                return
 
 if __name__ == '__main__':
-	pass
+	sd = MicArray()
+	time_var = time.time()
+	sd.run(start_time = time_var)
